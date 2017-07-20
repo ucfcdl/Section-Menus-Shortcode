81de1a8e8979dd6abe300e01a239a21ebe8167dd
RJ Bruneel <rj.bruneel@ucf.edu>
2017-07-20 09:34:29 -0400

    Updated drop shadow to only display when fixed top

--- includes/sections-menu-common.php
+++ includes/sections-menu-common.php
@@ -30,6 +30,7 @@ if ( ! class_exists( 'Section_Menus_Common' ) ) {
 		 **/
 		public static function enqueue_assets() {
 			wp_enqueue_script( 'section-menu-js', SECTION_MENUS__SCRIPT_URL . 'section-menu.min.js', array( 'jquery', 'script' ), null, true );
+			wp_enqueue_style( 'section-menu', plugins_url( 'static/css/section-menu.min.css', SECTION_MENUS__FILE ), null, false, 'screen' );
 		}
 
 		/**
@@ -53,7 +54,7 @@ if ( ! function_exists( 'section_menus_display_default' ) ) {
 	function section_menus_display_default( $selector ) {
 		ob_start();
 	?>
-		<nav class="navbar navbar-toggleable-md navbar-light bg-primary sections-menu" style="box-shadow: 0 2px 32px rgba(0,0,0,.5);">
+		<nav class="navbar navbar-toggleable-md navbar-light bg-primary sections-menu">
 			<div class="container">
 				<button class="navbar-toggler collapsed ml-auto" type="button" data-toggle="collapse" data-target="#sections-menu" aria-controls="#sections-menu" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="sr-only">Toggle sections navigation</span>
